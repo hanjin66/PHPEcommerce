6269b526b8c4ffc775f1414343dea8de6723deb5
hanjin66@gmail.com <h>
2018-02-28 04:11:43 -0700

    second commit

--- index.php
+++ index.php
@@ -1,42 +1,43 @@
 <?php
 
 require_once 'core/init.php';
-include 'includes/head.php'; 
+include 'includes/head.php';
 include 'includes/navigation.php';
 include 'includes/headerfull.php';
 include 'includes/leftbar.php';
 
 $sql = "SELECT * FROM products WHERE featured =1";
- ?>
+$featured = $db->query($sql);
+?>
 
 
 
 
 
-        
-        <!-- main content -->
-        <div class="col-md-8">Feature products
-            <div class="row">
-                <h2 class="text-center">Feature products</h2>
-                <div class="col-md-3">
-                    <h4>Levis Jean</h4>
-                    <img src="images/products/men4.png" alt="levis Jeans" class="img-thumb" />
-                    <p class="list-price text-danger"> List price:
-                        <s>54</s>
-                    </p>
-                    <p class="price">Our price: $19.99</p>
-                    <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Details</button>
-                </div>
-                
+    <!-- main content -->
+    <div class="col-md-8">Feature products
+        <div class="row">
+            <h2 class="text-center">Feature products</h2>
+            <?php while ($product = mysqli_fetch_assoc($featured)): ?>
+            <div class="col-md-3">
+                <h4><?= $product['title'];?> </h4>
+                <img src="<?=$product['image']; ?>" alt="<?= $product['title'];?>" class="img-thumb" />
+                <p class="list-price text-danger"> List price:
+                    <s><?=$product['list_price'];?></s>
+                </p>
+                <p class="price">Our price: $<?=$product['price'];?></p>
+                <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Details</button>
             </div>
+            <?php endwhile;?>
+
         </div>
-    
+    </div>
+
 
 
 
 <?php
-include 'includes/detailsmodal.php';
 include 'includes/rightbar.php';
 include 'includes/footer.php';
- 
- ?>
+
+?>
